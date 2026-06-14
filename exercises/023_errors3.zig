e1e4dfc5b320aa0b411112680fcd7f4c005605a1
sat0ken <15720506+sat0ken@users.noreply.github.com>
2026-06-14 21:18:36 +0900

    fix 023

--- exercises/023_errors3.zig
+++ exercises/023_errors3.zig
@@ -12,14 +12,14 @@ const MyNumberError = error{TooSmall};
 
 pub fn main() void {
     const a: u32 = addTwenty(44) catch 22;
-    const b: u32 = addTwenty(4) ??? 22;
+    const b: u32 = addTwenty(4) catch 22;
 
     std.debug.print("a={}, b={}\n", .{ a, b });
 }
 
 // この関数の戻り値の型を指定してください。
 // ヒント：エラーユニオンになります。
-fn addTwenty(n: u32) ??? {
+fn addTwenty(n: u32) MyNumberError!u32 {
     if (n < 5) {
         return MyNumberError.TooSmall;
     } else {
