ff2854b1296d204476dbedf0cb2ea74c51d9159b
sat0ken <15720506+sat0ken@users.noreply.github.com>
2026-07-17 00:12:12 +0900

    fix 037

--- exercises/037_structs.zig
+++ exercises/037_structs.zig
@@ -39,6 +39,7 @@ const Character = struct {
     role: Role,
     gold: u32,
     experience: u32,
+    health: u8,
 };
 
 pub fn main() void {
@@ -47,6 +48,7 @@ pub fn main() void {
         .role = Role.wizard,
         .gold = 20,
         .experience = 10,
+        .health = 100,
     };
 
     // Glorpは金を手に入れました。
