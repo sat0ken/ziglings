2baf17c421f1b797b465873164514bb8b5213cb1
sat0ken <15720506+sat0ken@users.noreply.github.com>
2026-07-16 22:12:22 +0900

    fix 036

--- exercises/036_enums2.zig
+++ exercises/036_enums2.zig
@@ -28,7 +28,7 @@ const std = @import("std");
 const Color = enum(u32) {
     red = 0xff0000,
     green = 0x00ff00,
-    blue = ???,
+    blue = 0x0000ff,
 };
 
 pub fn main() void {
@@ -50,12 +50,12 @@ pub fn main() void {
         \\<p>
         \\  <span style="color: #{x:0>6}">Red</span>
         \\  <span style="color: #{x:0>6}">Green</span>
-        \\  <span style="color: #{}">Blue</span>
+        \\  <span style="color: #{x:0>6}">Blue</span>
         \\</p>
         \\
     , .{
         @intFromEnum(Color.red),
         @intFromEnum(Color.green),
-        @intFromEnum(???), // おっと！何かが足りません！
+        @intFromEnum(Color.blue), // おっと！何かが足りません！
     });
 }
